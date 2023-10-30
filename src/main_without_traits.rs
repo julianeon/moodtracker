ca20e0300ce6abe2e6feded56bf41e7789c03599
julianeon <julianeon@gmail.com>
2023-10-30 08:51:25 -0700

    now w traits

--- src/main_without_traits.rs
+++ src/main_without_traits.rs
@@ -2,11 +2,6 @@ use chrono::{Local};
 use std::fs::{OpenOptions};
 use std::io::{Write};
 
-trait InputFeedback {
-    fn prompt_user(&mut self);
-    fn display_feedback(&self) -> String;
-}
-
 struct Exercise {
     did_exercise: bool,
 }
@@ -14,6 +9,31 @@ struct Exercise {
 impl Exercise {
     fn new() -> Self {
         Exercise { did_exercise: false }
+    }
+
+    fn prompt_user(&mut self) {
+        println!("Did you exercise? (y/n)");
+        let mut user_input = String::new();
+        std::io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
+
+        self.did_exercise = match user_input.trim().to_lowercase().as_str() {
+            "y" | "yes" => true,
+            "n" | "no" => false,
+            _ => {
+                println!("I don't know what that means so I'm assuming you didn't exercise.");
+                false
+            }
+        };
+    }
+
+    fn display_feedback(&self) -> String {
+        if self.did_exercise {
+            "You did exercise.".to_string()
+        } else {
+            "You did not exercise.".to_string()
+        }
     }
 }
 
@@ -32,45 +52,7 @@ impl Mood {
     fn new() -> Self {
         Mood { feeling: Feelings::Neutral }
     }
-}
 
-struct WorkCompleted {
-    count: i32,
-}
-
-impl WorkCompleted {
-    fn new() -> Self {
-        WorkCompleted { count: 0 }
-    }
-}
-
-impl InputFeedback for Exercise {
-    fn prompt_user(&mut self) {
-        println!("Did you exercise? (y/n)");
-        let mut user_input = String::new();
-        std::io::stdin()
-            .read_line(&mut user_input)
-            .expect("Failed to read line");
-
-        self.did_exercise = match user_input.trim().to_lowercase().as_str() {
-            "y" | "yes" => true,
-            "n" | "no" => false,
-            _ => {
-                println!("I don't know what that means so I'm assuming you didn't exercise.");
-                false
-            }
-        };
-    }
-    fn display_feedback(&self) -> String {
-        if self.did_exercise {
-            "You did exercise.".to_string()
-        } else {
-            "You did not exercise.".to_string()
-        }
-    }
-}
-
-impl InputFeedback for Mood {
     fn prompt_user(&mut self) {
         println!("How are you feeling today?");
         println!("Options: Happy, Sad, Anxious, Neutral");
@@ -91,6 +73,7 @@ impl InputFeedback for Mood {
             }
         };
     }
+
     fn display_feedback(&self) -> String {
         match self.feeling {
             Feelings::Happy => "You are feeling happy!".to_string(),
@@ -101,7 +84,15 @@ impl InputFeedback for Mood {
     }
 }
 
-impl InputFeedback for WorkCompleted {
+struct WorkCompleted {
+    count: i32,
+}
+
+impl WorkCompleted {
+    fn new() -> Self {
+        WorkCompleted { count: 0 }
+    }
+
     fn prompt_user(&mut self) {
         println!("How many work tasks did you complete today?");
         let mut work_input = String::new();
@@ -119,6 +110,7 @@ impl InputFeedback for WorkCompleted {
             }
         }
     }
+
     fn display_feedback(&self) -> String {
         format!("You completed {} work tasks so far today.", self.count)
     }
